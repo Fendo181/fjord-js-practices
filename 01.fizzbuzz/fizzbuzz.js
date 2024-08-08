4d9eefb9500d928527ef5fa7f90df4f2e3d1e734
Fendo181 <fendo181git@gmail.com>
2024-08-08 09:48:37 +0900

    add: shebangで実行するためにコマンドを追加

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
+
 for (let step = 1; step <= 20; step++) {
-  if (step % 15 === 0) {
+  if ((step % 3 === 0) && (step % 5 === 0)) {
     console.log("FizzBuzz");
   } else if (step % 3 === 0) {
     console.log("Fizz");
