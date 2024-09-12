dd677721af33bca1e249086fbbc198d56d3baf6c
Fendo181 <fendo181git@gmail.com>
2024-09-12 09:38:17 +0900

    minimistでコマンドラインから引数を受け取れるように処理を追加した

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,16 +1,7 @@
 #!/usr/bin/env node
 
-function showUsageAndExit() {
-  const path = require('path');
-  const basename = path.basename(process.argv[1]);
-  console.error(`Usage: ${basename} [year] [month]`);
-  process.exit(1);
-}
+import minimist from 'minimist';
 
-const args = process.argv.slice(2);
-if (args.length < 1) {
-  showUsageAndExit();
-}
-
-const name = args[0];
-console.log(`Hello, ${name}!`);
+const args = minimist(process.argv.slice(2));
+const month = args['m']
+const year = args['y']
